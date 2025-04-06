3788fccb29b484310abace8daba025def8503cff
vikraman-mani <vikraman02032001@gmail.com>
2025-04-06 11:48:13 +0530

    List Rendering

--- to-do-app/src/components/Content.jsx
+++ to-do-app/src/components/Content.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Content = () => {
+
+  let [items,setItems] = useState([
+    {id: 1, lable:"Html & Css", required: true},
+    {id: 2, lable:"JavaScript", required: true},
+    {id: 3, lable:"React", required: false},
+  ])
+
   return (
-    <div>Content</div>
-  )
+    <main>
+     <ul>
+        {
+            items.map( (item)=> {
+                return (
+                    <li key={item.id}>
+                        <input type="checkbox" checked = {item.required} />
+                        <lable> {item.lable}</lable>
+                    </li>
+                )
+            })
+        }
+     </ul>
+    </main>
+   )
 }
 
 export default Content
