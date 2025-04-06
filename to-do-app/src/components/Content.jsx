e532b5d18f6d45e1abe652420066754c059bfa7d
vikraman-mani <vikraman02032001@gmail.com>
2025-04-06 13:36:00 +0530

    final Applicatiion using CRUD

--- to-do-app/src/components/Content.jsx
+++ to-do-app/src/components/Content.jsx
@@ -12,6 +12,10 @@ const Content = () => {
 
   let [newItem, setNewItem] = useState("")
 
+  let [isEdit, setIsEdit] = useState(false)
+
+  let [currentId, setCurrentId] = useState(null)
+
   let handleChecked = (id) => {
     let newListItems = items.map( (item)=> {
         return item.id === id ? {...item, required: !item.required} : item
@@ -20,13 +24,47 @@ const Content = () => {
     setItems(newListItems)
   }
 
+  let handleUpdate = (id) => {
+     setIsEdit(true)
+
+     setCurrentId(id)   // Kept Update ID
+
+     let listIem = items.find( item => item.id === id)
+     setNewItem(listIem.label)
+  }
+
+  let handleDelete = (id) => {
+    let newListItems = items.filter( (item)=> 
+        item.id !== id ).map( (item,index) => {
+            return {...item, id: index + 1 }
+        } )
+    setItems(newListItems)
+  }
+
+  let handleAddOrSave = () => {
+    if(isEdit) {
+      let newListItems = items.map( (item)=> {
+        return item.id == currentId ? {...item, label: newItem} : item
+      })
+      setItems(newListItems)
+
+      setCurrentId(null)
+      setNewItem("")
+      setIsEdit(false)
+    }
+    else {
+      setItems( [...items, {id: items.length + 1, label: newItem, required: false} ] )
+      setNewItem("")
+    }
+  }
+
   return (
     <main>
 
     <div>
         <input type="text" placeholder='Add new item' value={newItem} 
         onChange={(event)=> { setNewItem(event.target.value) } } />
-        <button>ADD </button>
+        <button onClick={ handleAddOrSave }> {isEdit ? "Save" : "ADD"} </button>
     </div>
      <ul>
         {
@@ -35,8 +73,8 @@ const Content = () => {
                     <li key={item.id} className='item'>
                         <input type="checkbox" checked = {item.required} onChange={ () => { handleChecked(item.id) } } />
                         <label> {item.label}</label>
-                        <FaEdit role='button' tabIndex={0}/>
-                        <RiDeleteBin6Fill role='button' tabIndex={0} />
+                        <FaEdit role='button' tabIndex={0} onClick={ ()=> {handleUpdate(item.id)} }/>
+                        <RiDeleteBin6Fill role='button' tabIndex={0} onClick={ ()=> {handleDelete(item.id)}} />
                     </li>
                 )
             })
